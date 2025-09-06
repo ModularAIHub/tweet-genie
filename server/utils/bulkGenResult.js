dcc45e7507da09c27626ba1cf3c9718161393ae4
kanishk2404 <saraswatkanishk24.com>
2025-09-06 22:02:26 +0530

    redist reverted to use previous method for now

--- server/utils/bulkGenResult.js
+++ server/utils/bulkGenResult.js
@@ -1,6 +1,7 @@
 import { getRedisClient } from '../config/bulkGenQueue.js';
 
 const redis = getRedisClient();
+redis.connect();
 
 export const getBulkGenResult = async (jobId) => {
   const data = await redis.get(`bulkgen:result:${jobId}`);
